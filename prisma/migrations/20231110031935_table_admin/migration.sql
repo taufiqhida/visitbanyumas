9f10b56055d38ec388819977915155ed5c569b5a
taufiqhida <taufiqhida>
2023-11-10 10:26:52 +0700

    generate admin

---
+++ prisma/migrations/20231110031935_table_admin/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "admin" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "admin_pkey" PRIMARY KEY ("id")
+);
