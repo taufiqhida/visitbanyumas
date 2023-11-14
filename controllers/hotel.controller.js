bd69fcc78aa8607a59935b68249aa93b0df6617d
taufiqhida <taufiqhida>
2023-11-14 15:05:57 +0700

    UPDATE wisata

--- controllers/hotel.controller.js
+++ controllers/hotel.controller.js
@@ -6,6 +6,13 @@ module.exports={
             const {title, deskripsi, linkmap, alamat, nohp, harga_min, harga_max} = req.body
             const newHotel = await hotel.create({
                 data : {
+                    title,
+                    deskripsi,
+                    linkmap,
+                    alamat,
+                    nohp,
+                    harga_min,
+                    harga_max,
                     
                 }
             })
