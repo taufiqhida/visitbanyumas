4ae601fba09aa61367a73de80907a66de8c22a40
taufiqhida <taufiqhida>
2023-12-06 21:01:00 +0700

    perbaikan schema

--- prisma/migrations/20231113163429_update_hotel/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hotelId` on the `wisatas` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "wisatas" DROP COLUMN "hotelId";
